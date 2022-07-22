55eae845b6ed6f8c4c0a9df8ab9601f2c2e1349e
Jasmine <66095189+jasminect08@users.noreply.github.com>
2022-07-22 13:20:55 -0400

    Update mobile-nav-jquery.php

--- mobile-nav-jquery.php
+++ mobile-nav-jquery.php
@@ -1,1 +1,5 @@
-
+// load the JS file
+function wdm_mm_toggle_scripts() {
+    wp_enqueue_script( 'wdm-mm-toggle', get_stylesheet_directory_uri() . '/js/mobile-menu-toggle.js', array('jquery') );
+}
+add_action( 'wp_enqueue_scripts', 'wdm_mm_toggle_scripts' );
